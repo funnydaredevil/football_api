e484a64ab222df37eb4a3c5a7adbff07541bd953
Oleg Okunevych <okunevichov@gmail.com>
2018-08-29 14:01:22 +0300

    Get rid of if condition

---
+++ src/lib/football_api_web/cached_resource.ex
@@ -0,0 +1,27 @@
+defmodule FootballApi.CachedResource do
+  @moduledoc """
+    Module for handling cached resources
+  """
+  def load_resource(resource, params, get_resource_function) do
+    key = build_cache_key(resource, params)
+    Cachex.exists?(:my_cache, key)
+    |> get_resource(key, params, get_resource_function)
+  end
+
+  defp get_resource({:ok, true}, key, _, _) do
+    Cachex.get!(:my_cache, key)
+  end
+
+  defp get_resource(_, key, params, get_resource_function) do
+    loaded = get_resource_function.(params)
+    Cachex.put(:my_cache, key, loaded)
+    loaded
+  end
+
+  defp build_cache_key(resource, params) do
+    query_string = params
+    |> Enum.map( fn({key, value}) -> "#{key}=#{value}" end)
+    |> Enum.join("&")
+    "#{resource}&#{query_string}"
+  end
+end
